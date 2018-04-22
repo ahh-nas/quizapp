fbc115d312b562176b65334fa44df7490465a836
Adrianne Williams <adriannew816@gmail.com>
2018-04-22 14:06:15 -0400

    created ConnectionManager class

---
+++ Quiz App/ConnectionManager.swift
@@ -0,0 +1,15 @@
+//
+//  ConnectionManager.swift
+//  Quiz App
+//
+//  Created by Adrianne Williams on 4/22/18.
+//  Copyright © 2018 edu.self. All rights reserved.
+//
+
+import Foundation
+import MultipeerConnectivity
+
+class ConnectionManager : NSObject {
+    
+    
+}
