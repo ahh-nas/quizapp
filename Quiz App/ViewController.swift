0e7b88fb424c6264aa56f1070ad73a406efb384d
AnasZafar <zafarma2@vcu.edu>
2018-04-26 10:23:05 -0400

    The Peer to Peer works!

--- Quiz App/ViewController.swift
+++ Quiz App/ViewController.swift
@@ -9,25 +9,115 @@
 import UIKit
 import MultipeerConnectivity
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessionDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    let gameService = ConnectionManager()
+    var session: MCSession!
+    var peerID: MCPeerID!
+    
+    var browser: MCBrowserViewController!
+    var assistant: MCAdvertiserAssistant!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-       
+        
+        
+        self.peerID = MCPeerID(displayName: UIDevice.current.name)
+        self.session = MCSession(peer: peerID)
+        self.browser = MCBrowserViewController(serviceType: "chat", session: session)
+        self.assistant = MCAdvertiserAssistant(serviceType: "chat", discoveryInfo: nil, session: session)
+        
+        assistant.start()
+        session.delegate = self
+        browser.delegate = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    
+   
+    @IBAction func connect(_ sender: UIButton) {
+    
+        present(browser, animated: true, completion: nil)
+        
     }
-
-    @IBAction func pressedStart(_ sender: UIButton) {
-        let quizStoryboard = UIStoryboard(name: "QuizView", bundle: .main)
-        if let quizVC = quizStoryboard.instantiateViewController(withIdentifier: "QuizView") as? QuizViewController {
-            self.present(quizVC, animated: true, completion: nil)
+    
+    
+    //**********************************************************
+    // required functions for MCBrowserViewControllerDelegate
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        // Called when the browser view controller is dismissed
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        // Called when the browser view controller is cancelled
+        dismiss(animated: true, completion: nil)
+    }
+    //**********************************************************
+    
+    
+    
+    
+    //**********************************************************
+    // required functions for MCSessionDelegate
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        
+        print("inside didReceiveData")
+        
+        // this needs to be run on the main thread
+        DispatchQueue.main.async(execute: {
+            
+            if let receivedString = NSKeyedUnarchiver.unarchiveObject(with: data) as? String{
+                //self.updateChatView(newText: receivedString, id: peerID)
+            }
+            
+            if let image = UIImage(data: data) {
+                
+                //self.imgView.image = image
+                
+                //self.updateChatView(newText: "received image", id: peerID)
+                
+                
+            }
+            
+        })
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        
+        // Called when a connected peer changes state (for example, goes offline)
+        
+        switch state {
+        case MCSessionState.connected:
+            print("Connected: \(peerID.displayName)")
+            
+        case MCSessionState.connecting:
+            print("Connecting: \(peerID.displayName)")
+            
+        case MCSessionState.notConnected:
+            print("Not Connected: \(peerID.displayName)")
         }
+        
     }
+    //**********************************************************
+    
+  
+    
+    
 }
 
